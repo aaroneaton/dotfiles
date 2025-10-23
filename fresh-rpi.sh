4b0ef9092dd709a487a68cced4b5620647794259
J. Aaron Eaton <aaron@channeleaton.com>
2025-10-23 14:35:53 -0500

    Adding file for Raspberry Pi setup

---
+++ fresh-rpi.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+echo "Setting up your Raspberry Pi..."
+
+
+# Removes .vimrc from $HOME (if it exists) and symlinks the .vimrc file from the .dotfiles
+rm -rf $HOME/.vimrc
+ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+
+# Removes tmux configuration (if it exists) and symlinks .tmux.conf and .tmux folder from the .dotfiles
+rm -rf $HOME/.tmux.conf $HOME/.tmux
+ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.dotfiles/.tmux $HOME/.tmux
+
+# Update repositories
+sudo apt update
+
+# Install applications
+sudo apt install tmux
+
+sudo apt-get install -y silversearcher-ag zsh fonts-powerline build-essential
+
+# Change default shell to ZSH
+chsh -s $(which zsh)
+
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Create the swap and backup directories for vim
+mkdir $HOME/.vim/backups
+mkdir $HOME/.vim/swaps
+
+# Add the global .gitignore
+git config --global core.excludesfile ~/.dotfiles/.gitignore_global
